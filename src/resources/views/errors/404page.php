b9ee337ab440235481eaf1e2234446a74645b444
yoshidasyoki <yoshikingcorporation@gmail.com>
2026-09-12 18:14:41 +0900

    TailwindCSSの動作チェック完了

--- src/resources/views/errors/404page.php
+++ src/resources/views/errors/404page.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html lang="ja">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="resources/css/404page.css">
-    <title>存在しないページ</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <title>エラー</title>
 </head>
-
 <body>
-    <h2 class="text-3xl">404 Not Found</h2>
+    <div class="flex flex-col justify-center items-center h-screen">
+        <h2 class="text-3xl">404 Not Found</h2>
+        <p>アクセスいただいたページは存在しません。</p>
+    </div>
 </body>
-
 </html>
--- src/resources/views/errors/405page.php
+++ src/resources/views/errors/405page.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html lang="ja">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="resources/css/404page.css">
-    <title>不正なアクセス</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <title>エラー</title>
 </head>
-
 <body>
-    <h2>405 Method Not Allowed</h2>
+    <div class="flex flex-col justify-center items-center h-screen">
+        <h2 class="text-3xl">405 Method Not Allowed</h2>
+        <p>不正なアクセスが行われました。</p>
+    </div>
 </body>
-
 </html>
